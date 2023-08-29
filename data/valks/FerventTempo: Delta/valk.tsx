dca8b8ea07b6a471329e5773a0c365203e7d0c48
Haidar Chaito <114855311+HaidarChaitoMena@users.noreply.github.com>
2023-08-30 01:03:32 +0300

    fix spelling

--- data/valks/FerventTempo: Delta/valk.tsx
+++ data/valks/FerventTempo: Delta/valk.tsx
@@ -1,9 +1,9 @@
 import { Valkery } from '@/types/Valkery';
 import build1 from './build1';
 
-const FerventTempo: Delta: Valkery = {
-  label: 'FerventTempo: Delta',
+const FerventTempoDeltaValkery = {
+  label: 'FerventTempoDelta',
   image: 'https://user-images.githubusercontent.com/114035701/203185665-d64d2dfe-b830-4348-a685-6a1a71cbf644.png',
   build: [build1],
 };
-export default FerventTempo: Delta;
+export default FerventTempoDeltaValkery;
