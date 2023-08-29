820ae3d22213f20c7d287935faef7d005ad4a198
Haidar Chaito <114855311+HaidarChaitoMena@users.noreply.github.com>
2023-08-29 23:40:52 +0000

    create build 2

---
+++ data/valks/ChronoNavi/build2.tsx
@@ -0,0 +1,12 @@
+import KevinSignetOfDeliverance2ndCore from '@/data/signets/KevinSignetofDeliverance2ndCore.g';
+import { Build } from '@/types/Build';
+
+const build2: Build = {
+  label: 'Kevin build',
+  tier: 1,
+  isNew: true,
+  signets: [
+  ],
+};
+
+export default build2;
