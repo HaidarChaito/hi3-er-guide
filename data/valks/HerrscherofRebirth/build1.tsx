714ea267360388cfd1873f0628931624ff026943
AdoriZahard <114035701+AdoriZahard@users.noreply.github.com>
2023-08-30 00:39:10 +0000

    test

---
+++ data/valks/HerrscherofRebirth/build1.tsx
@@ -0,0 +1,11 @@
+import AponiaSignetOfDiscipline1stCore from '@/data/signets/AponiaSignetofDiscipline1stCore.g';
+import { Build } from '@/types/Build';
+
+const build1: Build = {
+  label: 'build 1',
+  tier: 1,
+  isNew: true,
+  signets: [],
+};
+
+export default build1;
--- data/visibleValks.tsx
+++ data/visibleValks.tsx
@@ -1,5 +1,6 @@
 import ArgentKnightArtemis from "./valks/ArgentKnightArtemis/valk";
 import ChronoNavi from "./valks/ChronoNavi/valk";
+import HerrscherofRebirth from "./valks/HerrscherofRebirth/valk";
 
 // Add valkeries here
-export const valks = [ArgentKnightArtemis,ChronoNavi];
+export const valks = [ArgentKnightArtemis, ChronoNavi, HerrscherofRebirth];
