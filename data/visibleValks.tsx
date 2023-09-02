e14898cc2647a99d15edb5f5ad883a1d633045cb
AdoriZahard <114035701+AdoriZahard@users.noreply.github.com>
2023-09-02 18:14:37 +0000

    test1

--- data/visibleValks.tsx
+++ data/visibleValks.tsx
@@ -1,7 +1,8 @@
 import ArgentKnightArtemis from './valks/ArgentKnightArtemis/valk';
 import ChronoNavi from './valks/ChronoNavi/valk';
 import HerrscherofHumanity from './valks/HerrscherofHumanity/valk';
+import HerrscherofOrigin from './valks/HerrscherofOrigin/valk';
 import HerrscherofRebirth from './valks/HerrscherofRebirth/valk';
 
 // Add valkeries here
-export const valks = [ArgentKnightArtemis, ChronoNavi, HerrscherofRebirth, HerrscherofHumanity];
+export const valks = [ArgentKnightArtemis, ChronoNavi, HerrscherofRebirth, HerrscherofHumanity, HerrscherofOrigin];
