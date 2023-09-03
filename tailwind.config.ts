f1fe4bd18c1b1459f1af688aef2a1dd914e9fa9d
HaidarChaitoMena <haidarchaito@icloud.com>
2023-09-04 01:07:25 +0300

    add react-icons + theme colors to tailwin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,18 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   important:true,
+  safelist: [
+    'border-elight',
+    'border-eice',
+    'border-ephys',
+    'border-efire',
+    'border-redprimary',
+    'text-elight',
+    'text-eice',
+    'text-ephys',
+    'text-efire',
+    'text-redprimary',
+  ],
   content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
@@ -12,6 +24,12 @@ const config: Config = {
       colors: {
         primary: '#07182e',
         secondary: '#f6f6f6',
+        redprimary: '#ea1179',
+        blueprimary: '#279eff',
+        ephys: '#EFB495',
+        eice: '#97FFF4',
+        efire: '#ED2B2A',
+        elight: "#F2BE22"
       },
     },
   },
