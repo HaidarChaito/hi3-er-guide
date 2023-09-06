bed348c23778db1f43382d5bcae57c5baa388a19
HaidarChaitoMena <haidarchaito@icloud.com>
2023-09-06 03:59:19 +0300

    add footer and copy right

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+export default function Footer() {
+
+
+    return (
+        <>
+            <div className="py-4 fixed text-center bottom-0 w-screen bg-primary text-white">
+                © 2023 HaidarChaitoMena. All rights reserved.
+            </div>
+        </>
+    );
+}
