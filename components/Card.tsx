ab417faef00f3f1e0b7f5f89e85ecd2404284df1
HaidarChaito <haidarchaito@icloud.com>
2023-09-09 01:28:57 +0300

    remove stretch from card img

--- components/Card.tsx
+++ components/Card.tsx
@@ -30,7 +30,7 @@ export default function Card({ valkery }: props) {
         }}
       >      <div className={`${styles.card} w-full pb-1 sm:w-64`}>
           <div className='z-10 w-full text-white'>
-            <Image objectFit="cover" className='h-48 w-full rounded-t-3xl	p-2' width={200} height={100} src={'/images/valks/' + valkery.label + '.webp'} alt={valkery.label} />
+            <Image objectFit="fill" className='h-48 w-full rounded-t-3xl	p-2' width={200} height={100} src={'/images/valks/' + valkery.label + '.webp'} alt={valkery.label} />
             <div className='py-1 text-center'>
               <span className='text-lg font-semibold'>{valkery.label}</span>
               <hr className={`border-1 border-${switchColor(valkery.type)} my-2`} />
@@ -43,7 +43,7 @@ export default function Card({ valkery }: props) {
         {isModalOpen && (
           <div className='fixed left-0	top-0 z-20 flex h-full w-full items-center justify-center overflow-auto backdrop-blur-sm backdrop-brightness-75 backdrop-saturate-50'>
             <dialog open id='myModal' className={`absolute z-10 block  h-4/5 w-4/5 rounded-md opacity-90 bg-primary text-secondary border-2 border-${switchColor(valkery.type)}`}>
-              <Image className="opacity-25 z-0" objectPosition="center" objectFit="cover" layout="fill" src={'/images/valks/' + valkery.label + '.webp'} alt={valkery.label} />
+              <Image className="opacity-25 z-0" objectPosition="center" objectFit="fill" layout="fill" src={'/images/valks/' + valkery.label + '.webp'} alt={valkery.label} />
               <div className='flex flex-col z-30 relative'>
                 <div className='flex h-auto w-full items-center justify-center my-2'>
                   <div className='flex h-auto w-full items-center justify-center text-lg font-bold sm:text-2xl'>{valkery.label}</div>
