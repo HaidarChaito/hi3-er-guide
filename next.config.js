5c1428e9ce150f0c92af73f3e8659c9ddc2bfe5c
HaidarChaito <haidarchaito@icloud.com>
2023-09-08 23:00:57 +0300

    remove akami image loader

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: '/hi3-er-guide',
   images: {
-    loader: 'akamai',
-    domains: ['user-images.githubusercontent.com'],
     path: '/hi3-er-guide',
   },
 };
