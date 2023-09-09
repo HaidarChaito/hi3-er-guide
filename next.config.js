d3773c6de3e2c121d301f0cdc63cafe6726a9408
HaidarChaito <haidarchaito@icloud.com>
2023-09-09 16:34:27 +0300

    remove github action deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/hi3-er-guide',
-  images: {
-    path: '/hi3-er-guide',
-    loader: 'akamai',
-  },
-  output:'export'
 };
 
 module.exports = nextConfig;
