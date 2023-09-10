51a3e0b75c2d0d70890e1a174323416d6e80805f
Adori Zahard <osoriokevin.m@Gmail.com>
2023-09-10 00:43:19 -0300

    valk images

--- data/valks/ChronoNavi/build1.tsx
+++ data/valks/ChronoNavi/build1.tsx
@@ -6,14 +6,15 @@ const build1: Build = {
   label: 'General Build',
   tier: 5,
   isNew: true,
+  notes: 'test',
   signets: [],
   supports: [
     [AvatarImages.Azure_Empyrea_Avatar, AvatarImages.Blood_Rose_Avatar],
     [AvatarImages.Azure_Empyrea_Avatar, AvatarImages.Bright_Knight_Excelsis_Avatar],
     [AvatarImages.Azure_Empyrea_Avatar, AvatarImages.Blood_Rose_Avatar],
-  ]
+  ],
   emblems: [
-    [EmblemsImages.Light_as_a_Bodhi_Leaf, EmblemsImages.Because_of_You],
+    [EmblemsImages.Light_as_a_Bodhi_Leaf, EmblemsImages.Because_of_You, EmblemsImages.Falling_in_Past_Light],
     [EmblemsImages.Pseudo_Miracle, EmblemsImages.Falling_in_Past_Light],
     [EmblemsImages.Tin_Flask, EmblemsImages.Falling_in_Past_Light],
   ]
