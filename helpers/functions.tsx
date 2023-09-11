51d4d8eada21ad4170cae0ce5f39b882bbcbdd5b
HaidarChaitoMena <haidarchaito@icloud.com>
2023-09-11 20:15:32 +0300

    create function to set priority for item without affecting the other users

--- helpers/functions.tsx
+++ helpers/functions.tsx
@@ -1,14 +1,22 @@
+import { Signet } from '@/types/Signet';
+
 export function switchColor(element?: number) {
-    switch (element) {
-        case (1):
-            return 'ephys';
-        case (2):
-            return 'eice';
-        case (3):
-            return 'elight';
-        case (4):
-            return 'efire';
-        default:
-            return 'redprimary';
-    }
+  switch (element) {
+    case 1:
+      return 'ephys';
+    case 2:
+      return 'eice';
+    case 3:
+      return 'elight';
+    case 4:
+      return 'efire';
+    default:
+      return 'redprimary';
+  }
+}
+
+export function withPriority(signet: Signet, priority?: string) {
+  const cln = structuredClone(signet);
+  cln.priority = priority ?? 'Optional';
+  return cln;
 }
