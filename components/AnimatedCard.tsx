d13eb98f2f6e02cb57b60900e1780294150e7252
HaidarChaitoMena <haidarchaito@icloud.com>
2023-09-13 03:13:47 +0300

    add shimmer loading placeholder for images

--- components/AnimatedCard.tsx
+++ components/AnimatedCard.tsx
@@ -24,6 +24,7 @@ export default function AnimatedCard({ valkery, setSelected, isRecommended }: pr
           src={'/static/images/valks/' + valkery.label + '.png'}
           loading='eager'
           alt={valkery.label}
+          placeholder={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
           onClick={() => setSelected(valkery)}
           fill
           sizes='(max-width: 768px) 100vw, 300px'
@@ -32,3 +33,20 @@ export default function AnimatedCard({ valkery, setSelected, isRecommended }: pr
     </div>
   );
 }
+
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+const toBase64 = (str: string) =>
+  typeof window === 'undefined' ? Buffer.from(str).toString('base64') : window.btoa(str);
