2c29425f7fa84606340546a905dde7253abaed05
HaidarChaitoMena <haidarchaito@icloud.com>
2023-09-25 15:58:51 +0300

    add website metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,30 @@ const nunito = Nunito({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Hi3 ER guide',
-  description: 'Honkai impact 3rd ER guide by HaidarChaitoMena',
+  description: 'Honkai impact 3rd ER guide by HaidarChaitoMena, AdoriZahar & AquaHi3',
+  applicationName: 'Hi3er',
+  authors: [
+    { name: 'HaidarChaitoMena', url: 'https://github.com/HaidarChaitoMena' },
+    { name: 'AdoriZahar', url: 'https://github.com/AdoriZahar' },
+    { name: 'AquaHi3', url: 'https://github.com/AquaHi3' },
+  ],
+  creator: 'HaidarChaitoMena',
+  publisher: 'HaidarChaitoMena',
+  openGraph: {
+    title: 'Hi3er',
+    description: 'Honkai impact 3rd ER guide by HaidarChaitoMena, AdoriZahar & AquaHi3',
+    url: 'https://hi3er.tech',
+    siteName: 'Hi3er',
+    images: [
+      {
+        url: 'https://images-ext-2.discordapp.net/external/fXsDf50jW_ShXu1Vf7OTbqW3jslN3ZqoTp_nijtD11o/https/cdn.discordapp.com/emojis/1134456907134926931.gif',
+        width: 400,
+        height: 400,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: '',
   images: {
-    domains: ["hi3er.tech","www.hi3er.tech"],
+    domains: ["hi3er.tech","www.hi3er.tech","images-ext-2.discordapp.net"],
   },
 };
 
