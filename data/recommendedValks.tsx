90c97bc52683619b1ff35183f15cd3ff6ac22442
Adori Zahard <osoriokevin.m@Gmail.com>
2023-09-26 23:15:34 -0300

    Updated Recommended Valks

--- data/recommendedValks.tsx
+++ data/recommendedValks.tsx
@@ -7,6 +7,12 @@ import Dreamweaver from './valks/Dreamweaver/valk';
 import VermilionKnightEclipse from './valks/VermilionKnightEclipse/valk';
 import { compareValkeriesTier } from '@/helpers/functions';
 import TerminalAide0017 from './valks/TerminalAide0017/valk';
+import FallenRosemary from './valks/FallenRosemary/valk';
+import HelicalContraption from './valks/HelicalContraption/valk';
+import HerrscherofHumanity from './valks/HerrscherofHumanity/valk';
+import HerrscherofOrigin from './valks/HerrscherofOrigin/valk';
+import HerrscherofTruth from './valks/HerrscherofTruth/valk';
+import JadeKnight from './valks/JadeKnight/valk';
 
 // Add Recommended here
 const recommendedValks: Valkery[] = [
@@ -17,6 +23,12 @@ const recommendedValks: Valkery[] = [
   Dreamweaver,
   VermilionKnightEclipse,
   TerminalAide0017,
+  FallenRosemary,
+  HelicalContraption,
+  HerrscherofHumanity,
+  HerrscherofOrigin,
+  HerrscherofTruth,
+  JadeKnight,
 ];
 
 recommendedValks.sort(compareValkeriesTier);
