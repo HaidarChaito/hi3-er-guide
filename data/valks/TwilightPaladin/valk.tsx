41bf8ade7cd032241d94799eacd4d46a4468a7f6
AquaHi3 <144223535+AquaHi3@users.noreply.github.com>
2023-11-20 11:37:27 +0000

    More phase 2 changes

--- data/valks/TwilightPaladin/valk.tsx
+++ data/valks/TwilightPaladin/valk.tsx
@@ -11,10 +11,10 @@ const TwilightPaladin: Valkery = {
   loadoutBuffs: [
     { title: 0, selected: '✓' },
     { title: 5, selected: ' ' },
-    { title: 5, selected: '' },
+    { title: 5, selected: '✓' },
+    { title: 10, selected: '✓' },
     { title: 10, selected: '' },
-    { title: 10, selected: '' },
-    { title: 15, selected: '✓' },
+    { title: 15, selected: '' },
     { title: 15, selected: '✓' },
   ],
 };
