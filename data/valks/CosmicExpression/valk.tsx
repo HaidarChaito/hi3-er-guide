da09428c8192207957fdc9dc44b27684ae40fa2e
HaidarChaito <haidarchaito@icloud.com>
2023-12-07 23:07:28 +0200

    add base CE files

---
+++ data/valks/CosmicExpression/valk.tsx
@@ -0,0 +1,21 @@
+import { Valkery } from '@/types/Valkery';
+import build1 from './build1';
+
+const ChronoNavi: Valkery = {
+  label: 'Cosmic Expression (CE)',
+  image: 'Cosmic Expression',
+  build: [build1],
+  minRank: 3,
+  type: 4,
+  tier: 5,
+  loadoutBuffs: [
+    { title: 0, selected: '✓' },
+    { title: 5, selected: ' ' },
+    { title: 5, selected: '✓ ' },
+    { title: 10, selected: '✓ ' },
+    { title: 10, selected: ' ' },
+    { title: 15, selected: '✓' },
+    { title: 15, selected: '' },
+  ],
+};
+export default ChronoNavi;
