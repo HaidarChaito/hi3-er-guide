0f9b79bd87e653395dffc5e1995fbd9278db9906
AquaHi3 <144223535+AquaHi3@users.noreply.github.com>
2023-12-10 13:47:24 +0000

    Cosmic Expression build

--- data/valks/CosmicExpression/build1.tsx
+++ data/valks/CosmicExpression/build1.tsx
@@ -1,22 +1,81 @@
 import { AvatarImages } from '@/data/supports/AvatarImages.g';
+import { Signets_of_Daybreak_Kosma } from '@/data/signets/extra/extra';
+import { Signets_of_Reverie_Pardofelis } from '@/data/signets/extra/extra';
+import { Signets_of_Setsuna_Sakura } from '@/data/signets/extra/extra';
+import { Signets_of_Stars_Griseo } from '@/data/signets/extra/extra';
+import { Signets_of_Infinity_Mobius } from '@/data/signets/extra/extra';
+import { Signets_of_Helix_VillV } from '@/data/signets/extra/extra';
+
+
 import { BuildType } from '@/types/Build';
+import { withPriority } from '@/helpers/functions';
+import { CosmicEmpression_Ego } from '@/data/signets/tsx/EgoAdditions';
 
 const build1: BuildType = {
-  label: 'General Build',
-  tier: 5,
+  label: 'Griseo Skywalker - Jedi Build',
+  tier: 4.5,
   gear: [],
   bottomNotes: [
     {
-      text: 'SEQUENCE:',
+      text: 'SEQUENCE: Dodge > WA + Ult > Dodge SEQ 4 Connect > WA - Repeat - Read notes.',
     },
   ],
   isNew: true,
-  notes: '',
-  signets: [],
+  notes: 'Ultimate Evasion connects to SEQ 4 as does parry stance, "Blessing of Kiku" from Sakura smoothens the rotation loop massively. DO NOTE - DO NOT PRESS WA, OTHERWISE YOU WILL LOSE OUT ON DPS. IT WILL SELF ACTIVATE!',
+  signets: [
+    [
+      withPriority(CosmicEmpression_Ego.Blessing_of_Anchor,'Start'),
+      withPriority(CosmicEmpression_Ego.Blessing_of_Dazzle,'Start'),
+      withPriority(CosmicEmpression_Ego.Blessing_of_Shining_Blade,'F2 Optional')
+    ],
+
+    [
+      Signets_of_Helix_VillV.Act_II_Pendulum,
+      Signets_of_Helix_VillV.Act_III_Conflict,
+      Signets_of_Helix_VillV.Act_I_Magic,
+      Signets_of_Helix_VillV.Act_IV_Spoon,
+      withPriority(Signets_of_Helix_VillV.Interlude_Reversed_Spiral, 'CORE'),
+      Signets_of_Helix_VillV.Act_VII_HalfClosed_Door,
+      Signets_of_Helix_VillV.Act_VIII_Adorned_Chamber,
+    ],
+
+    [
+      Signets_of_Daybreak_Kosma.Wings_that_Mask_the_Sanguine_Moon,
+      Signets_of_Daybreak_Kosma.Talons_that_Desecrate_the_Santuary_sic,
+      Signets_of_Daybreak_Kosma.Horns_that_Rip_the_Bleak_Sky,
+      Signets_of_Daybreak_Kosma.Eyes_that_Overlook_the_Vicious_Abyss,
+      withPriority(Signets_of_Daybreak_Kosma.Heart_that_Committed_Seven_Crimes, 'Optional'),
+      withPriority(Signets_of_Daybreak_Kosma.Daybreak_an_Undying_Light, 'CORE'),
+      Signets_of_Daybreak_Kosma.Undetermined_Judgement_sic,
+      Signets_of_Daybreak_Kosma.Unwavering_Promise,
+    ],
+    [
+      Signets_of_Infinity_Mobius.Entwined_P,
+      Signets_of_Infinity_Mobius.Lip_Poison_E,
+      Signets_of_Infinity_Mobius.Rodent_V,
+      withPriority(Signets_of_Infinity_Mobius.Infinite_X, 'CORE'),
+      Signets_of_Infinity_Mobius.Dead_X,
+    ],
+
+    [
+      Signets_of_Reverie_Pardofelis.Motto_of_Meow,
+      Signets_of_Reverie_Pardofelis.BackCurled_Tail,
+      Signets_of_Reverie_Pardofelis.Neighborhood_Chatters,
+      withPriority(Signets_of_Reverie_Pardofelis.Empty_Dreams_Self_Set_and_Joy, 'CORE'),
+    ],
+    [
+      withPriority(Signets_of_Setsuna_Sakura.Bountiful_Blossom_Kiku, 'Must get'),
+      withPriority(Signets_of_Stars_Griseo.Red__Hot, 'Must Get'),
+      Signets_of_Stars_Griseo.Yellow__Warm,
+      Signets_of_Setsuna_Sakura.Bountiful_Blossom_Koyo,
+      Signets_of_Setsuna_Sakura.Bountiful_Blossom_Botan,
+    ],
+
+  ],
   supports: [
-    [AvatarImages.Azure_Empyrea_Avatar, AvatarImages.Blood_Rose_Avatar],
-    [AvatarImages.Azure_Empyrea_Avatar, AvatarImages.Bright_Knight_Excelsis_Avatar],
-    [AvatarImages.Azure_Empyrea_Avatar, AvatarImages.Blood_Rose_Avatar],
+    [AvatarImages.Starlit_Astrologos_Avatar, AvatarImages.Blood_Rose_Avatar],
+    [AvatarImages.Starlit_Astrologos_Avatar, AvatarImages.Bright_Knight_Excelsis_Avatar],
+    [AvatarImages.Starlit_Astrologos_Avatar, AvatarImages.Blood_Rose_Avatar],
   ],
   emblems: [],
 };
--- data/valks/CosmicExpression/build2.tsx
+++ data/valks/CosmicExpression/build2.tsx
@@ -1,22 +1,78 @@
 import { AvatarImages } from '@/data/supports/AvatarImages.g';
+import { Signets_of_Deliverance_Kevin } from '@/data/signets/extra/extra';
+import { Signets_of_Reverie_Pardofelis } from '@/data/signets/extra/extra';
+import { Signets_of_Stars_Griseo } from '@/data/signets/extra/extra';
+import { Signets_of_Infinity_Mobius } from '@/data/signets/extra/extra';
+import { Signets_of_Helix_VillV } from '@/data/signets/extra/extra';
+import { Signets_of_Setsuna_Sakura } from '@/data/signets/extra/extra';
 import { BuildType } from '@/types/Build';
+import { CosmicEmpression_Ego } from '@/data/signets/tsx/EgoAdditions';
+import { withPriority } from '@/helpers/functions';
 
 const build2: BuildType = {
-  label: 'General Build',
-  tier: 5,
+  label: 'Parry Build',
+  tier: 2,
   gear: [],
-  bottomNotes: [
+  bottomNotes: [ 
     {
-      text: 'SEQUENCE:',
+      text: 'SEQUENCE: WA > Hold your parry button > While holding down the parry button, press attack for Combo 4 when the timefracture begins > Repeat',
     },
   ],
   isNew: true,
-  notes: '',
-  signets: [],
+  notes: 'Disclaimer this build feels janky is extremely weaker than the other, although looking braindead, this build needs to pay attention to the time fracture as it can be easily mistimed, ruining the SEQ 4 connect; Take BKE if you find yourself struggling with Heimdal',
+  signets: [
+[
+  withPriority(CosmicEmpression_Ego.Blessing_of_Meteorite, 'Start'),
+  withPriority(CosmicEmpression_Ego.Blessing_of_Starlight, 'Start'),
+  withPriority(CosmicEmpression_Ego.Blessing_of_Shining_Blade, 'F2 Optional'),
+],
+[
+  Signets_of_Helix_VillV.Act_II_Pendulum,
+  Signets_of_Helix_VillV.Act_III_Conflict,
+  Signets_of_Helix_VillV.Act_I_Magic,
+  Signets_of_Helix_VillV.Act_IV_Spoon,
+  withPriority(Signets_of_Helix_VillV.Interlude_Reversed_Spiral, 'CORE'),
+  Signets_of_Helix_VillV.Act_VII_HalfClosed_Door,
+  Signets_of_Helix_VillV.Act_VIII_Adorned_Chamber,
+],
+[
+  Signets_of_Deliverance_Kevin.Goblet_of_the_Giver,
+  Signets_of_Deliverance_Kevin.Artifact_of_the_Inhibitor,
+  Signets_of_Deliverance_Kevin.Brand_of_the_Undead,
+  withPriority(Signets_of_Deliverance_Kevin.Blade_of_the_Deliverer, 'CORE'),
+  Signets_of_Deliverance_Kevin.Crusade_of_the_Deliverer,
+],
+
+
+ [Signets_of_Infinity_Mobius.Entwined_P,
+  Signets_of_Infinity_Mobius.Lip_Poison_E,
+  Signets_of_Infinity_Mobius.Rodent_V,
+  withPriority(Signets_of_Infinity_Mobius.Infinite_X, 'CORE'),
+  Signets_of_Infinity_Mobius.Dead_X,
+],
+
+[
+  Signets_of_Reverie_Pardofelis.Motto_of_Meow,
+  Signets_of_Reverie_Pardofelis.BackCurled_Tail,
+  Signets_of_Reverie_Pardofelis.Neighborhood_Chatters,
+  withPriority(Signets_of_Reverie_Pardofelis.Empty_Dreams_Self_Set_and_Joy, 'CORE'),
+],
+
+[
+  withPriority(Signets_of_Stars_Griseo.Red__Hot, 'Must Get'),
+  Signets_of_Stars_Griseo.Yellow__Warm,
+  Signets_of_Setsuna_Sakura.Bountiful_Blossom_Koyo,
+  Signets_of_Setsuna_Sakura.Bountiful_Blossom_Botan,
+],
+
+  
+],
+
+  
   supports: [
-    [AvatarImages.Azure_Empyrea_Avatar, AvatarImages.Blood_Rose_Avatar],
-    [AvatarImages.Azure_Empyrea_Avatar, AvatarImages.Bright_Knight_Excelsis_Avatar],
-    [AvatarImages.Azure_Empyrea_Avatar, AvatarImages.Blood_Rose_Avatar],
+    [AvatarImages.Starlit_Astrologos_Avatar, AvatarImages.Blood_Rose_Avatar],
+    [AvatarImages.Starlit_Astrologos_Avatar, AvatarImages.Bright_Knight_Excelsis_Avatar],
+    [AvatarImages.Starlit_Astrologos_Avatar, AvatarImages.Blood_Rose_Avatar],
   ],
   emblems: [],
 };
