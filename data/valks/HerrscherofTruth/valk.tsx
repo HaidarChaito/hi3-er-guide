f071ae7ffb897414c6baa4a3a5730ce03fad3b04
Haidar Chaito <114855311+HaidarChaitoMena@users.noreply.github.com>
2024-01-24 22:39:20 +0200

    fix htr loadout

--- data/valks/HerrscherofTruth/valk.tsx
+++ data/valks/HerrscherofTruth/valk.tsx
@@ -10,7 +10,7 @@ const HerrscherofTruth: valkeryType = {
   tier: 5,
   min_rank: 3,
   loadout_buffs: [
-    { title: 0, selected: 's' },
+    { title: 0, selected: '' },
     { title: 5, selected: '✓' },
     { title: 5, selected: '' },
     { title: 10, selected: '✓' },
